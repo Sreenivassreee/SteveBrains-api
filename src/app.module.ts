71d8f4526c64692065e9752a637a477fe44e10fe
BestBusiness-ae <bestbguae2dev@gmail.com>
2023-05-16 00:39:26 +0530

    initial commit

---
+++ src/app.module.ts
@@ -0,0 +1,14 @@
+import { Module } from '@nestjs/common';
+import { GraphQLModule } from '@nestjs/graphql';
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
+@Module({
+  imports: [
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver: ApolloDriver,
+      playground: false,
+    }),
+  ],
+  controllers: [],
+  providers: [],
+})
+export class AppModule {}
